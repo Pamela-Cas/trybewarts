bbd787f3599e487971737e4487eb92cb435a3327
Aline <allinne.oliveira.ol@gmail.com>
2021-12-02 19:28:35 -0300

    requisito 18 n funciona

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
 const btnEntrar = document.getElementById('entrar');
+const inputSim = document.getElementById('agreement');
+const btnEnviar = document.getElementById('submit-btn');
 function alerta() {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
     alert('Olá, Tryber!');
@@ -9,3 +11,10 @@ function alerta() {
   }
 }
 btnEntrar.addEventListener('click', alerta);
+
+if ( inputSim.disabled === true) {
+    btnEnviar.disabled = true;
+  } else if (inputSim.disabled === false){
+    btnEnviar.disabled = false;
+  }
+
